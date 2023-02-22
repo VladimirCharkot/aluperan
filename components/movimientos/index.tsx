96d711ef836b21cf411f09d1150e19f858b18f8e
VladimirCharkot <vlad.chk@gmail.com>
2023-02-21 21:29:12 -0300

    Total estable en movimientos

--- components/movimientos/index.tsx
+++ components/movimientos/index.tsx
@@ -1,7 +1,7 @@
-import { Movimiento } from "../../lib/api";
+import { Movimiento, MovimientoGenerico } from "../../lib/api";
 import { CartaBalance } from "./balance";
 import { ModalMovimiento } from "../general/modalMovimientos";
-import { startOfMonth, endOfMonth, isBefore, isAfter } from "date-fns";
+import { startOfMonth, endOfMonth, isBefore, isAfter, isEqual } from "date-fns";
 
 import { Lista } from "../general/lista";
 import { Carta } from "../general/carta";
@@ -18,6 +18,31 @@ export default function Movimientos({ movimientos }: MovimientosProps) {
   const [desde, setDesde] = useState(startOfMonth(new Date()))
   const [hasta, setHasta] = useState(endOfMonth(new Date()))
 
+  const antes = (m: Movimiento) => isBefore(new Date(m.fecha), desde) 
+  const despues = (m: Movimiento) => isAfter(new Date(m.fecha), hasta) 
+
+  
+  const entre_fechas = (m: Movimiento) => {
+    const f = new Date(m.fecha)
+    return (isBefore(f, hasta) && isAfter(f, desde)) || isEqual(f, desde) || isEqual(f, hasta)
+  }
+  
+  const anteriores: MovimientoGenerico = {
+    medio: '-',
+    fecha: desde,
+    razon: 'otra',
+    monto: movimientos.filter(antes).reduce((prev, curr) => prev + curr.monto, 0),
+    detalle: '[Movimientos anteriores]'
+  }
+  const posteriores: MovimientoGenerico = {
+    medio: '-',
+    fecha: new Date(),
+    razon: 'otra',
+    monto: movimientos.filter(despues).reduce((prev, curr) => prev + curr.monto, 0),
+    detalle: '[Movimientos posteriores]'
+  }
+  const movimientos_en_rango = sortBy([anteriores, ...movimientos.filter(entre_fechas), posteriores], m => m.fecha)
+
   return (
     <Lista titulo="Movimientos" bg="grilla">
       <div className="mx-6 flex justify-between w-72">
@@ -27,7 +52,7 @@ export default function Movimientos({ movimientos }: MovimientosProps) {
       {ingresando && <ModalMovimiento cerrar={() => setIngresando(false)}/>}
       <Carta>
         <Boton texto="Ingresar" color="indigo" onClick={() => setIngresando(true)}/>
-        <CartaBalance movimientos={sortBy(movimientos.filter(m => isBefore(new Date(m.fecha), hasta) && isAfter(new Date(m.fecha), desde)), m => m.fecha)} />
+        <CartaBalance movimientos={movimientos_en_rango} />
       </Carta>
     </Lista>
   );
