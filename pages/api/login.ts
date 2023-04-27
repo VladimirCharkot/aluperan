867197978d662b351d54f1093f14e83fe98f4cec
VladimirCharkot <vlad.chk@gmail.com>
2023-04-27 15:47:40 -0300

    Fix

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -27,18 +27,20 @@ async function login(req: NextApiRequest, res: NextApiResponse) {
     }
   }
 
-  if (req.method == 'PUT' && req.session.user && req.session.user.isLoggedIn) {
-    try {
-      const { new_password } = await req.body
-      await update_password(new_password)
-      res.json({ok: true})
-    } catch (e) {
-      console.log(`Error en password update:`)
-      console.log((e as Error).message)
-      res.status(500).json({ ok: false, message: (e as Error).message })
+  if (req.method == 'PUT') {
+    if(req.session.user && req.session.user.isLoggedIn){
+      try {
+        const { new_password } = await req.body
+        await update_password(new_password)
+        res.json({ok: true})
+      } catch (e) {
+        console.log(`Error en password update:`)
+        console.log((e as Error).message)
+        res.status(500).json({ ok: false, message: (e as Error).message })
+      }
+    }else{
+      res.status(401).json({message: 'No autorizado'})
     }
-  }else{
-    res.status(401).json({message: 'No autorizado'})
   }
 
   if(req.method != 'POST' && req.method != 'PUT') res.status(401).json({message: 'No autorizado'})
