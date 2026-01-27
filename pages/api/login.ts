553933c702fb3b7b77b0dbc6d7656bcf6f1cded0
Vlad <vlad.chk@gmail.com>
2026-01-27 12:36:39 -0300

    cerrar login

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,10 +1,9 @@
 import { withIronSessionApiRoute } from 'iron-session/next'
 import { sessionOptions } from '../../lib/session'
-import { NextApiRequest, NextApiResponse } from "next"
+import { NextApiRequest, NextApiResponse } from 'next'
 import { check_password, update_password } from '../../lib/db/password'
 
 async function login(req: NextApiRequest, res: NextApiResponse) {
-
   if (req.method == 'POST') {
     try {
       const { password } = await req.body
@@ -21,7 +20,6 @@ async function login(req: NextApiRequest, res: NextApiResponse) {
       req.session.user = user
       await req.session.save()
       res.json(user)
-
     } catch (error) {
       console.log(`Error en login:`)
       console.log((error as Error).message)
@@ -30,24 +28,22 @@ async function login(req: NextApiRequest, res: NextApiResponse) {
   }
 
   if (req.method == 'PUT') {
-    if(req.session.user && req.session.user.isLoggedIn){
+    if (req.session.user && req.session.user.isLoggedIn) {
       try {
         const { new_password } = await req.body
         await update_password(new_password)
-        res.json({ok: true})
+        res.json({ ok: true })
       } catch (e) {
         console.log(`Error en password update:`)
         console.log((e as Error).message)
         res.status(500).json({ ok: false, message: (e as Error).message })
       }
-    }else{
-      res.status(401).json({message: 'No autorizado'})
+    } else {
+      res.status(401).json({ message: 'No autorizado' })
     }
   }
 
-  if(req.method != 'POST' && req.method != 'PUT') res.status(401).json({message: 'No autorizado'})
-
-
+  if (req.method != 'POST' && req.method != 'PUT') res.status(401).json({ message: 'No autorizado' })
 }
 
 export default withIronSessionApiRoute(login, sessionOptions)
